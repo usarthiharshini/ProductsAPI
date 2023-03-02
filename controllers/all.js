d3c62e7dceefb96d7e72a28076aa9b418056b084
usarthiharshini <usarthiharshini@gmail.com>
2023-03-02 18:58:20 +0530

    api done

--- controllers/all.js
+++ controllers/all.js
@@ -34,30 +34,5 @@ try {
        })
 }
 }
-  /*  const month_name = req.params.month;
-   
 
-
-   const statistics_url = `https://distinct-hosiery-tuna.cyclic.app/statistics/${month_name}`
-   const barchart_url = `https://distinct-hosiery-tuna.cyclic.app/barchart/${month_name}`
-   const piechart_url = `https://distinct-hosiery-tuna.cyclic.app/piechart/${month_name}`
-
-
-   const statistics_data = await axios.get(statistics_url)
-   const barchart_data = await axios.get(barchart_url)
-   const piechart_data = await axios.get(piechart_url)
-
-
- 
-
-res.send(
-    { month : month_name,
-     statistics:   statistics_data.data.statistics  ,
-    barchart:    barchart_data.data.barchart,
-    piechart:    piechart_data.data.piechart
-}
-)
-
-}
- */
 module.exports = allFun;
