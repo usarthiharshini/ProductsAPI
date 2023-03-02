1d48231b60815fed941fcfe81858061697622456
usarthiharshini <usarthiharshini@gmail.com>
2023-03-02 18:56:28 +0530

    api done

--- controllers/all.js
+++ controllers/all.js
@@ -1,7 +1,40 @@
 const { default: axios } = require('axios');
 
 const allFun = async(req,res)=>{
-   const month_name = req.params.month;
+
+try {
+    const month_name = req.params.month;
+   
+
+
+    const statistics_url = `https://distinct-hosiery-tuna.cyclic.app/statistics/${month_name}`
+    const barchart_url = `https://distinct-hosiery-tuna.cyclic.app/barchart/${month_name}`
+    const piechart_url = `https://distinct-hosiery-tuna.cyclic.app/piechart/${month_name}`
+ 
+ 
+    const statistics_data = await axios.get(statistics_url)
+    const barchart_data = await axios.get(barchart_url)
+    const piechart_data = await axios.get(piechart_url)
+ 
+ 
+  
+ 
+ res.send(
+     { month : month_name,
+      statistics:   statistics_data.data.statistics  ,
+     barchart:    barchart_data.data.barchart,
+     piechart:    piechart_data.data.piechart
+ }
+ )
+ 
+ 
+} catch (error) {
+    res.status(500).json({
+        message: "Something went wrong"
+       })
+}
+}
+  /*  const month_name = req.params.month;
    
 
 
@@ -26,5 +59,5 @@ res.send(
 )
 
 }
-
+ */
 module.exports = allFun;
